200d496a2f5316f595aee67d2ef9b040a6aec94a
Meili Vanegas-Hernandez <mvanegas10@uniandes.edu.co>
2024-11-09 12:11:39 +0100

    Change height

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const DOWN = 'down'
 const UP = 'up'
 const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-const h = 400
-// Fix/unfix chart
+
+/**
+ * Hiddes/Shows header.
+*/
 new Waypoint({
     element: document.getElementById('experience'),
     handler: direction => {
@@ -22,7 +24,11 @@ new Waypoint({
     offset: `66%`
 })
 
+/**
+ * Shows go down button.
+*/
+
 setTimeout(() => {
     d3.select('#transition-title').attr('class', 'grey darken-4 btn-floating btn-large scale-transition scale-in')
-}, 1000)
+}, 500)
 
