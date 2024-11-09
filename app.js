60d4962161bd0909fd3fd2f73bef099f37b7a9a0
Meili Vanegas-Hernandez <mvanegas10@uniandes.edu.co>
2024-11-09 11:38:24 +0100

    Add feature discovery

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const DOWN = 'down'
 const UP = 'up'
 const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 const h = 400
-console.log(vh,30*vh/100)
 // Fix/unfix chart
 new Waypoint({
     element: document.getElementById('experience'),
@@ -11,13 +10,19 @@ new Waypoint({
             console.log(DOWN)
             d3.select('#title').style('visibility', 'hidden')
             d3.select('#header').style('visibility', 'visible')
-
+            d3.select('#transition-title').attr('class', 'grey darken-4 btn-floating btn-large scale-transition scale-out')
         }
         else if (direction === UP) {
             console.log(UP)
             d3.select('#title').style('visibility', 'visible')
             d3.select('#header').style('visibility', 'hidden')
-        }
+        d3.select('#transition-title').attr('class', 'grey darken-4 btn-floating btn-large scale-transition scale-in')
+    }
     },
-    offset: `30%`
+    offset: `66%`
 })
+
+setTimeout(() => {
+    d3.select('#transition-title').attr('class', 'grey darken-4 btn-floating btn-large scale-transition scale-in')
+}, 1000)
+
